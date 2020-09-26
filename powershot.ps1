23df0281a29bd37065f714d2d5529e1805847140
justin <justinwhitaker@protonmail.com>
2020-09-26 10:11:39 -0400

    updates

--- powershot.ps1
+++ powershot.ps1
@@ -1,52 +1,83 @@
-$cache_md5 = @{}
-$cache_sha1 = @{}
-
-function getHashes($path){
-    if(!$path){
+function get-snapshot(){
+    $cache_md5 = @{};
+    $cache_sha1 = @{};
+    function Get-SnapshotHashes($path){
+        if(!$path){
+            return @{
+                'MD5' = '';
+                'SHA1' = '';
+            }
+        }
+        $retMD5 = '';
+        $retSHA1 = '';
+        if($cache_md5.containsKey($path)){
+            $retMD5 = $cache_md5[$path]
+        }
+        else{
+            $retMD5 =  get-filehash -path $path -algorithm MD5
+            $cache_md5[$path] = $retMD5
+        }
+        if($cache_sha1.containsKey($path)){
+            $retSHA1 = $cache_sha1[$path]
+        }
+        else{
+            $retSHA1 =  get-filehash -path $path -algorithm SHA1
+            $cache_sha1[$path] = $retSHA1
+        }
         return @{
-            'MD5' = '';
-            'SHA1' = '';
+            'MD5' = $retMD5;
+            'SHA1' = $retSHA1;
         }
     }
-    $retMD5 = '';
-    $retSHA1 = '';
-    if($cache_md5.containsKey($path)){
-        $retMD5 = $cache_md5[$path]
-    }
-    else{
-        $retMD5 =  get-filehash -path $path -algorithm MD5
-        $cache_md5[$path] = $retMD5
-    }
-    if($cache_sha1.containsKey($path)){
-        $retSHA1 = $cache_sha1[$path]
-    }
-    else{
-        $retSHA1 =  get-filehash -path $path -algorithm SHA1
-        $cache_sha1[$path] = $retSHA1
-    }
-    return @{
-        'MD5' = $retMD5;
-        'SHA1' = $retSHA1;
+    foreach($process in get-process){
+        $name = $process.ProcessName
+        $path = $process.Path
+        $modules = foreach($module in $process.Modules){
+            $moduleProperties = @{
+                'Name' = $module.ModuleName;
+                'Path' = $module.FileName;
+                'Hashes' = Get-SnapshotHashes($module.FileName);
+            }
+            $moduleReturnObject = New-Object PSObject -Property $moduleProperties
+            write-output $moduleReturnObject
+        }
+        $properties = @{
+            'Name' = $name;
+            'Path' = $path;
+            'Hashes' = get-snapshothashes($path);
+            'Modules' = $modules;
+        }
+        $retval = New-Object PSObject -Property $properties
+        write-output $retval
     }
 }
+function Get-SnapshotPaths($snapshot){
+    $snapshot | 
+        Sort-Object -Unique -Property Path |
+        Select-Object -ExpandProperty Path |
+        Write-Output
+}
 
-foreach($process in get-process){
-    $name = $process.ProcessName
-    $path = $process.Path
-    $modules = foreach($module in $process.Modules){
-        $moduleProperties = @{
-            'Name' = $module.ModuleName;
-            'Path' = $module.FileName;
-            'Hashes' = getHashes($module.FileName);
-        }
-        $moduleReturnObject = New-Object PSObject -Property $moduleProperties
-        write-output $moduleReturnObject
-    }
+function Get-SnapshotModules($process){
+    $process | 
+        Sort-Object -Unique -Property Path | 
+        Select-Object -ExpandProperty Path |
+        Write-Output
+}
+
+function Get-SnapshotNewProcesses($reference, $difference){
+    $referenceProcessPaths = get-SnapshotPaths($referenceSnapshot)
+    $differenceProcessPaths = get-SnapshotPaths($differenceSnapshot)
+    $newProcesses = Compare-Object $referenceProcessPaths $differenceProcessPaths |
+        Where-Object -Property SideIndicator -Eq '=>' |
+        Select-Object -ExpandProperty InputObject
+    write-output $newProcesses
+}
+
+function Compare-Snapshot($referenceSnapshot, $differenceSnapshot){
     $properties = @{
-        'Name' = $name;
-        'Path' = $path;
-        'Hashes' = getHashes($path);
-        'Modules' = $modules;
+        'NewProcesses' = Get-SnapshotNewProcesses;
+        'NewModules' = @();
     }
     $retval = New-Object PSObject -Property $properties
     write-output $retval
